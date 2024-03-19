76544b1a4b1906b84d996e71cc763f349ceb5203
emrahhakan <emrahhakanagan@gmail.com>
2024-03-19 23:05:56 +0300

    initial commit

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,118 @@
+package org.example;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final int port;
+    private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png"
+            , "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html"
+            , "/events.html", "/events.js");
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(64);
+    private ServerPropertiesLoading serverPropertiesLoading;
+
+    public Server() {
+        serverPropertiesLoading = new ServerPropertiesLoading();
+        this.port = serverPropertiesLoading.getPort();
+    }
+
+    public void startServer() {
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final var socket = serverSocket.accept();
+                threadPool.submit(() -> handleConnection(socket));
+            }
+        } catch (IOException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void handleConnection(Socket socket) {
+        try (
+                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var out = new BufferedOutputStream(socket.getOutputStream());
+        ) {
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                socket.close();
+                return;
+            }
+
+            final var path = parts[1];
+            if (!validPaths.contains(path)) {
+                sendNotFound(out);
+                return;
+            }
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+
+            if (path.equals("/classic.html")) {
+                sendClassicResponse(filePath, mimeType, out);
+            } else {
+                sendOKResponse(filePath, mimeType, out);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private void sendNotFound(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+
+
+    private void sendClassicResponse(Path filePath, String mimeType, BufferedOutputStream out) throws IOException {
+        final var template = Files.readString(filePath);
+        final var content = template.replace(
+                "{time}",
+                LocalDateTime.now().toString()
+        ).getBytes();
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + content.length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.write(content);
+        out.flush();
+    }
+
+    private void sendOKResponse(Path filePath, String mimeType, BufferedOutputStream out) throws IOException {
+        final var length = Files.size(filePath);
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        Files.copy(filePath, out);
+        out.flush();
+    }
+
+}
