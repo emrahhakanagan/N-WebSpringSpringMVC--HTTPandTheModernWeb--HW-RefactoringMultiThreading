feb20e39503dae3d6552bab8254004db9d193d89
emrahhakan <emrahhakanagan@gmail.com>
2024-03-25 02:38:51 +0300

    unnecessery codes deleted

--- src/main/java/org/example/ServerStart.java
+++ src/main/java/org/example/ServerStart.java
@@ -1,20 +1,14 @@
 package org.example;
 
-import java.io.BufferedOutputStream;
-
 public class ServerStart {
     public static void main(String[] args) {
         final var server = new Server();
 
-        server.addHandler("GET", "/messages", new Handler() {
-            public void handle(Request request, BufferedOutputStream responseStream) {
-                // TODO: handlers code
-            }
+        server.addHandler("GET", "/messages", (request, responseStream) -> {
+            // TODO: handlers code
         });
-        server.addHandler("POST", "/messages", new Handler() {
-            public void handle(Request request, BufferedOutputStream responseStream) {
-                // TODO: handlers code
-            }
+        server.addHandler("POST", "/messages", (request, responseStream) -> {
+            // TODO: handlers code
         });
 
         server.listen();
