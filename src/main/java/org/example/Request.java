f4eb78c754d2f6e659d59448f49576aa1d1b859d
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-03-29 11:29:28 +0300

    functionality parsing body request for header Content-Type added

--- src/main/java/org/example/Request.java
+++ src/main/java/org/example/Request.java
@@ -4,27 +4,70 @@ import org.apache.hc.core5.http.NameValuePair;
 import org.apache.hc.core5.net.URLEncodedUtils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Request {
     private final String method;
     private final String path;
     private final String pathWithoutQS;
     private final Map<String, String> headers;
-    private final BufferedReader body;
+    private final BufferedReader in;
     private final List<NameValuePair> params;
+    private final Map<String, List<String>> postParams = new HashMap<>();
 
-    public Request(String method, String path, Map<String, String> headers, BufferedReader body) {
+    public Request(String method, String path, Map<String, String> headers, BufferedReader in) {
         this.method = method;
         this.path = path;
         this.headers = headers;
-        this.body = body;
+        this.in = in;
         this.pathWithoutQS = getOnlyPathWithoutQS(path);
         this.params = parsQueryString(path);
+
+        if (method.equalsIgnoreCase("POST") && headers.containsKey("Content-Type")) {
+            parseBody(in, headers.get("Content-Type"));
+        }
+    }
+
+    private void parseBody(BufferedReader in, String contentType) {
+        if ("application/x-www-form-urlencoded".equalsIgnoreCase(contentType)) {
+            StringBuilder bodyBuilder = new StringBuilder();
+            String line;
+            while (true) {
+                try {
+                    if (!((line = in.readLine()) != null && (!line.isEmpty()))) break;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                bodyBuilder.append(line);
+            }
+
+            String body = URLDecoder.decode(bodyBuilder.toString(), Charset.forName("UTF-8"));
+            Arrays.stream(body.split("&"))
+                    .map(param -> param.split("="))
+                    .forEach(parts -> {
+                        if (parts.length == 2) {
+                            postParams.computeIfAbsent(parts[0], k -> new ArrayList<>()).add(parts[1]);
+                        }
+                    });
+        }
+    }
+
+    public String getPostParam(String name) {
+        List<String> values = postParams.get(name);
+
+        if (values != null && !values.isEmpty()) {
+            return values.get(0);
+        }
+        return null;
+    }
+
+    public Map<String, List<String>> getPostParams() {
+        return Collections.unmodifiableMap(postParams);
     }
 
     private List<NameValuePair> parsQueryString(String path) {
@@ -64,8 +107,8 @@ public class Request {
         return headers;
     }
 
-    public BufferedReader getBody() {
-        return body;
+    public BufferedReader getIn() {
+        return in;
     }
 
     public String getPathWithoutQS() {
